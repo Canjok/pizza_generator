1d0705afd9ab8b998ea02529b3cea0b50a4acca3
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-06 16:48:00 +0200

    [15] Added settings button

--- pizza_generator/lib/features/pizza_generator/view/pizza_generator_page.dart
+++ pizza_generator/lib/features/pizza_generator/view/pizza_generator_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_generator/features/pizza_generator/bloc/bloc.dart';
 import 'package:pizza_generator/features/pizza_generator/widgets/pizza_generator_body.dart';
+import 'package:pizza_generator/features/settings/view/settings_page.dart';
 import 'package:pizza_generator/l10n/l10n.dart';
 
 /// {@template pizza_generator_page}
@@ -24,6 +25,18 @@ class PizzaGeneratorPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text(context.l10n.appTitle),
+          actions: [
+            IconButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute<dynamic>(
+                  builder: (context) => const SettingsPage(),
+                ),
+              ),
+              icon: const Icon(
+                Icons.settings,
+              ),
+            ),
+          ],
         ),
         body: const PizzaGeneratorView(),
       ),
