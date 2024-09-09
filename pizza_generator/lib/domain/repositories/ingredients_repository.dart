b625634d5422a7e47e3ef5c000c8311456da827f
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-09 17:52:33 +0200

    [29] Add abstract ingredients repository

---
+++ pizza_generator/lib/domain/repositories/ingredients_repository.dart
@@ -0,0 +1,7 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:pizza_generator/core/failure.dart';
+import 'package:pizza_generator/domain/entities/ingredient.dart';
+
+abstract class IngredientsRepository {
+  TaskEither<Failure, List<Ingredient>> getExampleIngredients();
+}
