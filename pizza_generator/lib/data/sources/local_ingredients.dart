f240af1e11914455f67afb2666eb82499d9988dd
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-11 22:17:45 +0200

    [39] Added methods add ingredient and load ingredients

--- pizza_generator/lib/data/sources/local_ingredients.dart
+++ pizza_generator/lib/data/sources/local_ingredients.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:fpdart/fpdart.dart';
 import 'package:pizza_generator/core/failure.dart';
 import 'package:pizza_generator/data/models/ingredient_list_model.dart';
+import 'package:pizza_generator/data/models/ingredient_model.dart';
 import 'package:pizza_generator/data/sources/preference_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,6 +23,48 @@ final class LocalIngredients {
             .toList();
         await preferences.setStringList(_key, ingredientsJsonStringList);
         return unit;
+      },
+      (error, stackTrace) => UnknownFailure(),
+    );
+  }
+
+  TaskEither<Failure, IngredientListModel> addIngredient(
+    IngredientModel ingredient,
+  ) {
+    return TaskEither.tryCatch(
+      () async {
+        final stringList = preferences.getStringList(_key) ?? []
+          ..add(json.encode(ingredient.toJson()));
+
+        await preferences.setStringList(_key, stringList);
+
+        final jsonIngredients =
+            stringList.map((e) => json.decode(e) as Map<String, dynamic>);
+        final ingredients = jsonIngredients
+            .map(
+              IngredientModel.fromJson,
+            )
+            .toList();
+
+        return IngredientListModel(ingredients: ingredients);
+      },
+      (error, stackTrace) => UnknownFailure(),
+    );
+  }
+
+  TaskEither<Failure, IngredientListModel> loadIngredients() {
+    return TaskEither.tryCatch(
+      () async {
+        final stringList = preferences.getStringList(_key) ?? [];
+        final jsonIngredients =
+            stringList.map((e) => json.decode(e) as Map<String, dynamic>);
+        final ingredients = jsonIngredients
+            .map(
+              IngredientModel.fromJson,
+            )
+            .toList();
+
+        return IngredientListModel(ingredients: ingredients);
       },
       (error, stackTrace) => UnknownFailure(),
     );
