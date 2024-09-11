dd02000f18daca36e480d8ba69465494e8dffd8d
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-11 13:21:58 +0200

    [35] Added ingredient selection changed event

--- pizza_generator/lib/features/pizza_generator/bloc/pizza_generator_event.dart
+++ pizza_generator/lib/features/pizza_generator/bloc/pizza_generator_event.dart
@@ -14,3 +14,13 @@ class LoadIngredientsEvent extends PizzaGeneratorEvent {
   /// {@macro custom_pizza_generator_event}
   const LoadIngredientsEvent();
 }
+
+class IngredientsSelectionChanged extends PizzaGeneratorEvent {
+  /// {@macro custom_pizza_generator_event}
+  const IngredientsSelectionChanged({
+    required this.name,
+    required this.isSelected,
+  });
+  final String name;
+  final bool isSelected;
+}
