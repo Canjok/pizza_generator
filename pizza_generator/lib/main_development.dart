dec13486349d4df59c9b95f9c204736721e93460
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-11 13:16:53 +0200

    [35] Added await and ensure widget initialized to all flavors

--- pizza_generator/lib/main_development.dart
+++ pizza_generator/lib/main_development.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:pizza_generator/app/app.dart';
 import 'package:pizza_generator/bootstrap.dart';
 import 'package:pizza_generator/injection_container.dart';
 
-void main() {
-  initServiceLocator();
-  bootstrap(() => const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServiceLocator();
+  await bootstrap(() => const App());
 }
