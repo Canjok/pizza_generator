a15d2c301fe561c5deef03f38adb27e900d6ae85
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-11 11:37:10 +0200

    [33] Added list view of ingredients

--- pizza_generator/lib/features/pizza_generator/widgets/pizza_generator_body.dart
+++ pizza_generator/lib/features/pizza_generator/widgets/pizza_generator_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_generator/features/pizza_generator/bloc/bloc.dart';
+import 'package:pizza_generator/features/pizza_generator/widgets/selectable_ingredient_widget.dart';
 
 /// {@template pizza_generator_body}
 /// Body of the PizzaGeneratorPage.
@@ -14,8 +15,30 @@ class PizzaGeneratorBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<PizzaGeneratorBloc, PizzaGeneratorState>(
       builder: (context, state) {
-        return Center(child: Text(state.customProperty));
+        return ListView.separated(
+          itemBuilder: (context, index) => ingredientBuilder(
+            context,
+            index,
+            state,
+          ),
+          separatorBuilder: (context, index) => const Divider(
+            height: 8,
+          ),
+          itemCount: state.ingredients.length,
+        );
       },
+    );
+  }
+
+  Widget ingredientBuilder(
+    BuildContext context,
+    int index,
+    PizzaGeneratorState state,
+  ) {
+    final ingredient = state.ingredients[index];
+    return SelectableIngredientWidget(
+      isSelected: ingredient.isSelected,
+      name: ingredient.name,
     );
   }
 }
