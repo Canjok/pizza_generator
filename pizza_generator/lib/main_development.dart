967e463af4e4a01ac56dedcead3a73722a32d400
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-11 11:29:14 +0200

    [33] Used injection container in all flavors

--- pizza_generator/lib/main_development.dart
+++ pizza_generator/lib/main_development.dart
@@ -1,6 +1,8 @@
 import 'package:pizza_generator/app/app.dart';
 import 'package:pizza_generator/bootstrap.dart';
+import 'package:pizza_generator/injection_container.dart';
 
 void main() {
+  initServiceLocator();
   bootstrap(() => const App());
 }
