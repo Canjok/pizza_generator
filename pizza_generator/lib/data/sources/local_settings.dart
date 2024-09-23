194812d66457d73320371c1ac10e464d83c1a741
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-23 19:16:13 +0200

    [55] Added local settings data source

---
+++ pizza_generator/lib/data/sources/local_settings.dart
@@ -0,0 +1,31 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:pizza_generator/core/failure.dart';
+import 'package:pizza_generator/data/sources/preference_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final class LocalSettings {
+  LocalSettings({required this.preferences});
+  final SharedPreferencesWithCache preferences;
+
+  final _key = PreferenceKeys.settings.name;
+
+  TaskEither<Failure, int> loadIngredientCountToGenerate() {
+    return TaskEither.tryCatch(
+      () async {
+        final count = preferences.getInt(_key) ?? 1;
+        return count;
+      },
+      (error, stackTrace) => UnknownFailure(),
+    );
+  }
+
+  TaskEither<Failure, Unit> saveIngredientCountToGenerate(int count) {
+    return TaskEither.tryCatch(
+      () async {
+        await preferences.setInt(_key, count);
+        return unit;
+      },
+      (error, stackTrace) => UnknownFailure(),
+    );
+  }
+}
