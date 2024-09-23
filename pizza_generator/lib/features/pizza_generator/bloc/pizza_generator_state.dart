1fd1c0a382aa74973ce07c4ed9a477a123e440aa
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-23 11:36:40 +0200

    [53] Added generated ingredients to state

--- pizza_generator/lib/features/pizza_generator/bloc/pizza_generator_state.dart
+++ pizza_generator/lib/features/pizza_generator/bloc/pizza_generator_state.dart
@@ -7,24 +7,28 @@ class PizzaGeneratorState extends Equatable {
   /// {@macro pizza_generator_state}
   const PizzaGeneratorState({
     required this.ingredients,
+    required this.generatedIngredients,
     this.customProperty = 'Default Value',
   });
 
   /// A description for customProperty
   final String customProperty;
   final List<Ingredient> ingredients;
+  final List<String> generatedIngredients;
 
   @override
-  List<Object> get props => [customProperty, ingredients];
+  List<Object> get props => [customProperty, ingredients, generatedIngredients];
 
   /// Creates a copy of the current PizzaGeneratorState with property changes
   PizzaGeneratorState copyWith({
     String? customProperty,
     List<Ingredient>? ingredients,
+    List<String>? generatedIngredients,
   }) {
     return PizzaGeneratorState(
       customProperty: customProperty ?? this.customProperty,
       ingredients: ingredients ?? this.ingredients,
+      generatedIngredients: generatedIngredients ?? this.generatedIngredients,
     );
   }
 }
@@ -34,5 +38,5 @@ class PizzaGeneratorState extends Equatable {
 /// {@endtemplate}
 class PizzaGeneratorInitial extends PizzaGeneratorState {
   /// {@macro pizza_generator_initial}
-  PizzaGeneratorInitial() : super(ingredients: []);
+  PizzaGeneratorInitial() : super(ingredients: [], generatedIngredients: []);
 }
