d75bed8183b0c895de085c810d02dfbfd90e2fd5
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-24 14:06:11 +0200

    [60] Added boolean allow repetitions to string generator

--- pizza_generator/lib/algorithms/string_generator.dart
+++ pizza_generator/lib/algorithms/string_generator.dart
@@ -1,21 +1,29 @@
 import 'dart:math';
 
 final class StringGenerator {
-  static List<String> generateStringsFromStringList(
-    List<String> strings,
-    int count,
-  ) {
+  static List<String> generateStringsFromStringList({
+    required List<String> strings,
+    required int count,
+    required bool allowRepetitions,
+  }) {
     if (strings.isEmpty || count <= 0) {
       return [];
     }
+    if (!allowRepetitions && strings.length <= count) {
+      return strings;
+    }
     final length = strings.length;
-    final generatedStrings = List<String>.empty(growable: true);
     final randomGenerator = Random();
+    final indexList = List<int>.empty(growable: true);
     for (var i = 0; i < count; i++) {
       final randomNumber = randomGenerator.nextInt(length);
-      generatedStrings.add(strings[randomNumber]);
+      if (allowRepetitions) {
+        indexList.add(randomNumber);
+      } else if (!allowRepetitions && !indexList.contains(randomNumber)) {
+        indexList.add(randomNumber);
+      }
     }
 
-    return generatedStrings;
+    return indexList.map((e) => strings[e]).toList();
   }
 }
