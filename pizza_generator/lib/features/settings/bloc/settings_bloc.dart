9c60acb089b75aa53fa2ccfe186791d38b95e68c
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-24 09:35:33 +0200

    [55] Adapted settings bloc to usecase changes

--- pizza_generator/lib/features/settings/bloc/settings_bloc.dart
+++ pizza_generator/lib/features/settings/bloc/settings_bloc.dart
@@ -1,31 +1,66 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:pizza_generator/domain/usecases/settings_usecases.dart';
 
 part 'settings_event.dart';
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
-  SettingsBloc() : super(const SettingsInitial()) {
+  SettingsBloc({
+    required this.saveIngredientGenerationCountUsecase,
+    required this.loadIngredientGenerationCountUsecase,
+  }) : super(const SettingsInitial()) {
     on<IncreaseIngredientCountEvent>(_onIncreaseIngredientCountEvent);
     on<DecreaseIngredientCountEvent>(_onDecreaseIngredientCountEvent);
+    on<LoadIngredientCountEvent>(_onLoadIngredientCountEvent);
   }
+
+  LoadIngredientGenerationCountUsecase loadIngredientGenerationCountUsecase;
+  SaveIngredientGenerationCountUsecase saveIngredientGenerationCountUsecase;
 
   FutureOr<void> _onIncreaseIngredientCountEvent(
     IncreaseIngredientCountEvent event,
     Emitter<SettingsState> emit,
-  ) {
-    final newCount = state.ingredientCountToGenerate + 1;
-    emit(state.copyWith(ingredientCountToGenerate: newCount));
+  ) async {
+    final previousCount = state.ingredientCountToGenerate;
+    final newCount = previousCount + 1;
+    final failureOrInt =
+        await saveIngredientGenerationCountUsecase(count: newCount).run();
+    failureOrInt.fold(
+      (l) => (),
+      (_) => emit(
+        state.copyWith(ingredientCountToGenerate: newCount),
+      ),
+    );
   }
 
   FutureOr<void> _onDecreaseIngredientCountEvent(
     DecreaseIngredientCountEvent event,
     Emitter<SettingsState> emit,
-  ) {
-    final newCount = state.ingredientCountToGenerate == 1
-        ? 1
-        : state.ingredientCountToGenerate - 1;
-    emit(state.copyWith(ingredientCountToGenerate: newCount));
+  ) async {
+    final previousCount = state.ingredientCountToGenerate;
+    final newCount = previousCount == 1 ? 1 : previousCount - 1;
+    final failureOrUnit =
+        await saveIngredientGenerationCountUsecase(count: newCount).run();
+    failureOrUnit.fold(
+      (l) => (),
+      (_) => emit(
+        state.copyWith(ingredientCountToGenerate: newCount),
+      ),
+    );
+  }
+
+  FutureOr<void> _onLoadIngredientCountEvent(
+    LoadIngredientCountEvent event,
+    Emitter<SettingsState> emit,
+  ) async {
+    final failureOrCount = await loadIngredientGenerationCountUsecase().run();
+    failureOrCount.fold(
+      (l) => (),
+      (count) => emit(
+        state.copyWith(ingredientCountToGenerate: count),
+      ),
+    );
   }
 }
