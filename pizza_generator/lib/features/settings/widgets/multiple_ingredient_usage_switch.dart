34f46f268ed977d427c79b46a20497ed0b1fcddc
Alex Schmidt <schmidt.alex91@gmx.de>
2024-09-26 16:42:46 +0200

    [62] Converted stateful widget to state less widget

--- pizza_generator/lib/features/settings/widgets/multiple_ingredient_usage_switch.dart
+++ pizza_generator/lib/features/settings/widgets/multiple_ingredient_usage_switch.dart
@@ -2,36 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:pizza_generator/features/pizza_generator/pizza_generator.dart';
 import 'package:pizza_generator/features/settings/bloc/settings_bloc.dart';
 
-class MultipleIngredientUsageSwitch extends StatefulWidget {
+class MultipleIngredientUsageSwitch extends StatelessWidget {
   const MultipleIngredientUsageSwitch({super.key});
 
   @override
-  State<MultipleIngredientUsageSwitch> createState() =>
-      _MultipleIngredientUsageSwitchState();
-}
-
-class _MultipleIngredientUsageSwitchState
-    extends State<MultipleIngredientUsageSwitch> {
-  bool isSelected = false;
-
-  @override
-  void initState() {
-    super.initState();
-    final settingsState = BlocProvider.of<SettingsBloc>(context).state;
-    isSelected = settingsState.allowMultipleUsageOfAnIngredient;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Switch.adaptive(
-      value: isSelected,
-      onChanged: (value) => setState(
-        () {
-          BlocProvider.of<SettingsBloc>(context).add(
-            SetAllowMultipleUsageOfAnIngredientEvent(allow: value),
-          );
-          isSelected = value;
-        },
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) => Switch.adaptive(
+        value: state.allowMultipleUsageOfAnIngredient,
+        onChanged: (value) => BlocProvider.of<SettingsBloc>(context).add(
+          SetAllowMultipleUsageOfAnIngredientEvent(allow: value),
+        ),
       ),
     );
   }
